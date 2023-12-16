fb235c0b7d7d5d89b29cfa03795ba544a9e00973
I.Safonov <safonovil17@yandex.ru>
2023-12-16 22:28:51 +0300

    Create a waiting list

--- index.ts
+++ index.ts
@@ -1,16 +1,33 @@
 import express from 'express'
 import { PrismaClient } from './node_modules/.prisma/client/index'
+import cors from 'cors'
 
 const prisma = new PrismaClient()
 const app = express()
 const port = 5000
 
+app.use(cors())
 app.use(express.json())
 
 app.post('/api', async (req, res) => {
-	console.log(req.body)
+	const { email, name } = req.body
 
-	res.json({ message: 'Hello world!' })
+	if (!email || !name) {
+		return res.status(400).json({ message: 'Name and email required fields!' })
+	}
+
+	try {
+		const createdEntry = await prisma.waitList.create({
+			data: {
+				email,
+				name,
+			},
+		})
+
+		res.json(createdEntry)
+	} catch (error) {
+		res.status(400).send({ message: error })
+	}
 })
 
 const server = app.listen(port, () => {
